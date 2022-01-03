362ebe1dbe0a3a242e59a89356fa6f902e7802ab
Gary Bouck <hgrunt54@gmail.com>
2022-01-03 01:42:53 -0600

    Skill changing works, plus added lots of images

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,23 +1,23 @@
 <template>
     <div class="Skills">
         <h1>Skills</h1>
-        <button class="heal" value="skill.heal" @click="showSkillSelection()">
+        <button class="heal" value="skill.heal" @click="showSkillSelection('heal')">
             <img id="heal" :src="skills.img_Heal" />
             <p>Heal</p>
         </button>
-        <button class="utility" value="skill.utility1" @click="showSkillSelection()">
+        <button class="utility" value="skill.utility1" @click="showSkillSelection('utility1')">
             <img id="utility1" :src="skills.img_Utility1" />
             <p>Utility 1</p>
         </button>
-        <button class="utility" value="skill.utility2" @click="showSkillSelection()">
+        <button class="utility" value="skill.utility2" @click="showSkillSelection('utility2')">
             <img id="utility2" :src="skills.img_Utility2" />
             <p>Utility 2</p>
         </button>
-        <button class="utility" value="skill.utility3" @click="showSkillSelection()">
+        <button class="utility" value="skill.utility3" @click="showSkillSelection('utility3')">
             <img id="utility3" :src="skills.img_Utility3" />
             <p>Utility 3</p>
         </button>
-        <button class="elite" value="skill.elite" @click="showSkillSelection()">
+        <button class="elite" value="skill.elite" @click="showSkillSelection('elite')">
             <img id="elite" :src="skills.img_Elite" />
             <p>Elite</p>
         </button>
@@ -26,21 +26,13 @@
     <div class="skills_selection" v-show="skillSelection">
         Skill Selections
         <p>
-            <img src="@/assets/images/skills/mesmer/utilities/Signet_of_Domination.png" @click="hideSkillSelection()" />
-            <img src="@/assets/images/skills/mesmer/utilities/Signet_of_Illusions.png" @click="hideSkillSelection()" />
-            <img src="@/assets/images/skills/mesmer/utilities/Signet_of_Inspiration.png" @click="hideSkillSelection()" />
-        </p>
-        <p>
-            <img src="@/assets/images/skills/mesmer/utilities/Signet_of_Midnight.png" @click="hideSkillSelection()" />
-            <img src="@/assets/images/skills/mesmer/utilities/Signet_of_Domination.png" @click="hideSkillSelection()" />
-            <img src="@/assets/images/skills/default/default_skill.png" @click="skillSelection = false" />
-        </p>
-        <p>
-            <img src="@/assets/images/skills/default/default_skill.png" @click="hideSkillSelection()" />
-            <img src="@/assets/images/skills/default/default_skill.png" @click="hideSkillSelection()" />
-            <img src="@/assets/images/skills/default/default_skill.png" @click="hideSkillSelection()" />
+            <img src="@/assets/images/skills/mesmer/utilities/Signet_of_Domination.png" @click="hideSkillSelection(this.changeSkill, 'Signet of Domination')" />
+            <img src="@/assets/images/skills/mesmer/utilities/Signet_of_Illusions.png" @click="hideSkillSelection(this.changeSkill, 'Signet of Illusions')" />
+            <img src="@/assets/images/skills/mesmer/utilities/Signet_of_Inspiration.png" @click="hideSkillSelection(this.changeSkill, 'Signet of Inspiration')" />
+            <img src="@/assets/images/skills/mesmer/utilities/Signet_of_Midnight.png" @click="hideSkillSelection(this.changeSkill, 'Signet of Midnight')" />
         </p>
     </div>
+
     <div>
         <h2 style="text-align: center;">The Currently Selected Skills are:</h2>
         <p>Heal: {{ this.skills.heal }}</p>
@@ -69,17 +61,74 @@
                     img_Elite: require('@/assets/images/skills/default/default_skill.png'),
                 },
                 skillSelection: false,
+                changeSkill: ""
             };
         },
         methods: {
-            showSkillSelection() {
+            showSkillSelection(sk) {
                 this.skillSelection = true;
+                this.changeSkill = sk;
             },
-            hideSkillSelection() {
-                this.skillSelection = false;
+            hideSkillSelection(sk, sel) {
+                for (let i = 0; i < skill.length; i++) {
+                    if (sel == skill[i].name) {
+                        if (sk == 'heal') {
+                            this.skills.heal = skill[i].name;
+                            this.skills.img_Heal = skill[i].img;
+                            this.skillSelection = false;
+                        }
+                        else if (sk == 'utility1') {
+                            this.skills.utility1 = skill[i].name;
+                            this.skills.img_Utility1 = skill[i].img;
+                            this.skillSelection = false;
+                        }
+                        else if (sk == 'utility2') {
+                            this.skills.utility2 = skill[i].name;
+                            this.skills.img_Utility2 = skill[i].img;
+                            this.skillSelection = false;
+                        }
+                        else if (sk == 'utility3') {
+                            this.skills.utility3 = skill[i].name;
+                            this.skills.img_Utility3 = skill[i].img;
+                            this.skillSelection = false;
+                        }
+                        else if (sk == 'elite') {
+                            this.skills.elite = skill[i].name;
+                            this.skills.img_Elite = skill[i].img;
+                            this.skillSelection = false;
+                        }
+                    }
+                }
             },
         }
     }
+
+    const skill = [
+        {
+            profession: 'Mesmer',
+            type: 'utility',
+            name: 'Signet of Domination',
+            img: require('@/assets/images/skills/mesmer/utilities/Signet_of_Domination.png')
+        },
+        {
+            profession: 'Mesmer',
+            type: 'utility',
+            name: 'Signet of Illusions',
+            img: require('@/assets/images/skills/mesmer/utilities/Signet_of_Illusions.png')
+        },
+        {
+            profession: 'Mesmer',
+            type: 'utility',
+            name: 'Signet of Inspiration',
+            img: require('@/assets/images/skills/mesmer/utilities/Signet_of_Inspiration.png')
+        },
+        {
+            profession: 'Mesmer',
+            type: 'utility',
+            name: 'Signet of Midnight',
+            img: require('@/assets/images/skills/mesmer/utilities/Signet_of_Midnight.png')
+        }
+    ];
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
---
+++ src/shared/data.js
@@ -0,0 +1,179 @@
+// JavaScript data file for objects
+
+const heals = [
+    {
+        id: 1,
+        profession: 'Mesmer',
+        name: 'Ether Feast',
+        img: require('@/assets/images/skills/mesmer/heals/Signet_of_Domination.png')
+    },
+    {
+        id: 2,
+        profession: 'Mesmer',
+        name: 'False Oasis',
+        img: require('@/assets/images/skills/mesmer/heals/False_Oasis.png')
+    },
+    {
+        id: 3,
+        profession: 'Mesmer',
+        name: 'Mantra of Recovery',
+        img: require('@/assets/images/skills/mesmer/heals/Mantra_of_Recovery.png')
+    },
+    {
+        id: 4,
+        profession: 'Mesmer',
+        name: 'Mirror',
+        img: require('@/assets/images/skills/mesmer/heals/Mirror.png')
+    },
+    {
+        id: 5,
+        profession: 'Mesmer',
+        name: 'Signet of the Ether',
+        img: require('@/assets/images/skills/mesmer/heals/Signet_of_the_Ether.png')
+    },
+    {
+        id: 6,
+        profession: 'Mesmer',
+        name: 'Twin Blade Restoration',
+        img: require('@/assets/images/skills/mesmer/heals/Twin_Blade_Restoration.png')
+    },
+    {
+        id: 7,
+        profession: 'Mesmer',
+        name: 'Well of Eternity',
+        img: require('@/assets/images/skills/mesmer/heals/Well_of_Eternity.png')
+    },
+    {
+        id: 8,
+        profession: 'Necromancer',
+        name: 'Consume Conditions',
+        img: require('@/assets/images/skills/necromancer/heals/Consume_Conditions.png')
+    },
+    {
+        id: 9,
+        profession: 'Necromancer',
+        name: 'Signet of Vampirism',
+        img: require('@/assets/images/skills/necromancer/heals/Signet_of_Vampirism.png')
+    },
+    {
+        id: 10,
+        profession: 'Necromancer',
+        name: 'Summon Blood Fiend',
+        img: require('@/assets/images/skills/necromancer/heals/Summon_Blood_Fiend.png')
+    },
+    {
+        id: 11,
+        profession: 'Necromancer',
+        name: 'Well of Blood',
+        img: require('@/assets/images/skills/necromancer/heals/Well_of_Blood.png')
+    },
+]
+
+const utilities = [
+    {
+        id: 1,
+        profession: 'Mesmer',
+        name: 'Signet of Domination',
+        img: require('@/assets/images/skills/mesmer/elites/Signet_of_Domination.png')
+    },
+    {
+        id: 2,
+        profession: 'Mesmer',
+        name: 'Signet of Illusions',
+        img: require('@/assets/images/skills/mesmer/elites/Signet_of_Illusions.png')
+    },
+    {
+        id: 3,
+        profession: 'Mesmer',
+        name: 'Signet of Inspiration',
+        img: require('@/assets/images/skills/mesmer/elites/Signet_of_Inspiration.png')
+    },
+    {
+        id: 4,
+        profession: 'Mesmer',
+        name: 'Signet of Midnight',
+        img: require('@/assets/images/skills/mesmer/elites/Signet_of_Midnight.png')
+    },
+    {
+        id: 5,
+        profession: 'Necromancer',
+        name: 'Plague Signet',
+        img: require('@/assets/images/skills/necromancer/elites/Plague_Signet.png')
+    },
+    {
+        id: 6,
+        profession: 'Necromancer',
+        name: 'Signet of Spite',
+        img: require('@/assets/images/skills/necromancer/elites/Signet_of_Spite.png')
+    },
+    {
+        id: 7,
+        profession: 'Necromancer',
+        name: 'Signet of the Locust',
+        img: require('@/assets/images/skills/mesmer/elites/Signet_of_the_Locust.png')
+    },
+    {
+        id: 8,
+        profession: 'Necromancer',
+        name: 'Signet of Undeath',
+        img: require('@/assets/images/skills/necromancer/elites/Signet_of_Undeath.png')
+    },
+
+]
+
+const elites = [
+    {
+        id: 1,
+        profession: 'Mesmer',
+        name: 'Gravity Well',
+        img: require('@/assets/images/skills/mesmer/elites/Gravity_Well.png')
+    },
+    {
+        id: 2,
+        profession: 'Mesmer',
+        name: 'Jaunt',
+        img: require('@/assets/images/skills/mesmer/elites/Juant.png')
+    },
+    {
+        id: 3,
+        profession: 'Mesmer',
+        name: 'Mass Invisibility',
+        img: require('@/assets/images/skills/mesmer/elites/Mass_Invisibility.png')
+    },
+    {
+        id: 4,
+        profession: 'Mesmer',
+        name: 'Signet of Humility',
+        img: require('@/assets/images/skills/mesmer/elites/Signet_of_Humility.png')
+    },
+    {
+        id: 5,
+        profession: 'Mesmer',
+        name: 'Thousand Cuts',
+        img: require('@/assets/images/skills/mesmer/elites/Thousand_Cuts.png')
+    },
+    {
+        id: 6,
+        profession: 'Mesmer',
+        name: 'Time Warp',
+        img: require('@/assets/images/skills/mesmer/elites/Time_Warp.png')
+    },
+    {
+        id: 7,
+        profession: 'Necromancer',
+        name: 'Lich Form',
+        img: require('@/assets/images/skills/necromancer/elites/Lich_Form.png')
+    },
+    {
+        id: 8,
+        profession: 'Necromancer',
+        name: 'Plaguelands',
+        img: require('@/assets/images/skills/necromancer/elites/Plaguelands.png')
+    },
+    {
+        id: 9,
+        profession: 'Necromancer',
+        name: 'Summon Flesh Golem',
+        img: require('@/assets/images/skills/necromancer/elites/Summon_Flesh_Golem.png')
+    }
+]
